4d4cc062ae29072bcf293699f19f8da8d68c65fd
YJ0922 <kh934124@gmail.com>
2024-12-10 17:25:37 +0800

    Add files via upload
    D:\sample_website_20241126\complete_frontend\my-app

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <Login/>
+</template>
+
+
+<script>
+import Login from './components/login.vue';
+
+
+export default {
+  name: 'App',
+  components: {
+    Login
+  }
+}
+</script>
+
+
+<style>
+
+
+</style>
